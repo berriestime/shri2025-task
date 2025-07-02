47e4eee9504c2c22f67998db4972f1bf70d6414f
Clukva <berries.whale@gmail.com>
2025-07-02 22:43:15 +0300

    feat: empty path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  base: "/shri2025-task/",
-  plugins: [react(), ghPages()],
+  base: '',
+  plugins: [react()],
   publicDir: 'public',
   build: {
     assetsInlineLimit: 0
