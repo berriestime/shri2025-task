e7937cc31aa0aa48144a814e009a49547b64718b
Clukva <berries.whale@gmail.com>
2025-07-03 12:06:02 +0300

    try to deploy without publicDir

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
-  plugins: [react()],
-  publicDir: 'public',
-  build: {
-    assetsInlineLimit: 0
-  }
+  plugins: [react()]
 })
